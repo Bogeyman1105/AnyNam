11bacfe03d6cec8a4ebef28164ae691eb0ce931d
Bogeyman1105 <108532940+Bogeyman1105@users.noreply.github.com>
2022-07-01 14:47:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// // alert('domManipulation')
+// const h1 = document.getElementsByTagName('h1')
+// const h2 = document.getElementsByTagName('h2')
+// const one = document.getElementsByClassName('one')
+// const oner = document.getElementById('one')
+
+// const h1 = document.querySelector('h1')
+// const h11 = document.querySelectorAll('h1')
+// const Nigeria = document.querySelector('.one')
+// const Nigerian = document.querySelectorAll('.one')
+// const Canada = document.querySelector('#one')
+// const Canadian = document.querySelectorAll('#one')
+
+// const p = document.querySelector('p')
+
+// const links = document.querySelectorAll('a')
+
+// for (let link of links){
+//     link.innerText='i am a link'
+// }
+
+// const h1 = document.querySelector('h1')
+// const img = document.querySelector('img')
+
+const p = document.querySelector('p')
